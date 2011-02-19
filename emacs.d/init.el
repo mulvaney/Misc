9c1d5f0202bb7d355186f7551f87572f7de06f06
Mike Mulvaney <mike.mulvaney@gmail.com>
2011-02-19 09:31:40 +0800

    added emacs

---
+++ emacs.d/init.el
@@ -0,0 +1,52 @@
+(push "/usr/local/bin" exec-path)
+
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq backup-by-copying t)
+(setq auto-save-list-file-prefix `(("." . "~/.emacs-auto-save-list/.saves-")))
+
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
+(setq-default indent-tabs-mode nil)
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)
+(blink-cursor-mode t)
+(show-paren-mode t)
+
+(set-fringe-style -1)
+(tooltip-mode -1)
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(inhibit-startup-screen t)
+ '(show-paren-mode t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+
+(put 'narrow-to-region 'disabled nil)
+
+;;; rhtml mode
+(add-to-list 'load-path "~/.emacs.d/rhtml")
+(require 'rhtml-mode)
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
+
