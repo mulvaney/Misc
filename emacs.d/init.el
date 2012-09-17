9b558da479c92ba73576d19327e2774314298062
Mike Mulvaney <mike.mulvaney@gmail.com>
2012-09-17 15:31:15 -0400

    fixed menu thing, took out some colors

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,6 +9,8 @@
   version-control t)
 (setq-default indent-tabs-mode nil)
 (setq inhibit-startup-message t)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
 
 (scroll-bar-mode -1)
 (blink-cursor-mode t)
@@ -27,12 +29,6 @@
  '(inhibit-startup-screen t)
  '(scroll-bar-mode (quote right))
  '(show-paren-mode t))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
 
 (put 'narrow-to-region 'disabled nil)
 
